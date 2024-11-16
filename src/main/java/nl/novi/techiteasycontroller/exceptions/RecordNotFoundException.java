0fe42742ca681894fcf3ae84039a76f7bb8ce8b8
AyoubNL <gmilio@live.nl>
2024-11-17 00:43:17 +0100

    Exceptionhandler

--- src/main/java/nl/novi/techiteasycontroller/exceptions/RecordNotFoundException.java
+++ src/main/java/nl/novi/techiteasycontroller/exceptions/RecordNotFoundException.java
@@ -2,12 +2,12 @@ package nl.novi.techiteasycontroller.exceptions;
 
 public class RecordNotFoundException extends RuntimeException {
 
-    public RecordNotFoundException(String message) {
-        super(message);
+    public RecordNotFoundException(){
+        super("Television id not found");
     }
 
-    public RecordNotFoundException(String message, Throwable cause) {
-        super(message, cause);
+    public RecordNotFoundException(String message) {
+        super(message);
     }
 
 }
