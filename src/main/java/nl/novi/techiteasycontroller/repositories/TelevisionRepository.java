fc0516abf37bc8f2f5c6b032a954c9b32074545b
AyoubNL <gmilio@live.nl>
2024-11-23 05:43:04 +0100

    Television entity finished

--- src/main/java/nl/novi/techiteasycontroller/repositories/TelevisionRepository.java
+++ src/main/java/nl/novi/techiteasycontroller/repositories/TelevisionRepository.java
@@ -1,4 +1,7 @@
 package nl.novi.techiteasycontroller.repositories;
 
-public interface TelevisionRepository {
+import nl.novi.techiteasycontroller.models.Television;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TelevisionRepository extends JpaRepository<Television, Long> {
 }
