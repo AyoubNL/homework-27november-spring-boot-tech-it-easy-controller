9f03df1b914b2ad5decbbb8bcdf7c399638589d4
AyoubNL <gmilio@live.nl>
2024-12-01 05:08:16 +0100

    Added Validation

--- src/main/java/nl/novi/techiteasycontroller/dtos/TelevisionInputDto.java
+++ src/main/java/nl/novi/techiteasycontroller/dtos/TelevisionInputDto.java
@@ -1,11 +1,20 @@
 package nl.novi.techiteasycontroller.dtos;
 
+
+import jakarta.validation.constraints.*;
+
 public class TelevisionInputDto {
 
+    @NotNull
     public String type;
+    @NotNull
     public String brand;
+    @Size(min = 2, max = 59)
     public String name;
+    @Positive
+    @Max(value = 10000)
     public Double price;
+    @Max(value = 170)
     public Double availableSize;
     public Integer refreshRate;
     public String screenType;
@@ -16,6 +25,7 @@ public class TelevisionInputDto {
     public Boolean hdr;
     public Boolean bluetooth;
     public Boolean ambiLight;
+    @Min(value = 1)
     public Integer originalStock;
     public Integer sold;
 
