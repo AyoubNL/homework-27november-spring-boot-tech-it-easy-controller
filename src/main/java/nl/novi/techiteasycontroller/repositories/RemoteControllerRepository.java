849a47040ce983e1b7e4226518040dab6b740c2c
AyoubNL <gmilio@live.nl>
2024-12-06 00:54:50 +0100

    Created repositories

---
+++ src/main/java/nl/novi/techiteasycontroller/repositories/RemoteControllerRepository.java
@@ -0,0 +1,7 @@
+package nl.novi.techiteasycontroller.repositories;
+
+import nl.novi.techiteasycontroller.models.RemoteController;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RemoteControllerRepository extends JpaRepository<RemoteController, Long> {
+}
